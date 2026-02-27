7d782b68197d5cc8e1bb381c68b2f28934ffc942
dlwlsdnr <dlwlsdnr24@gmail.com>
2026-02-28 00:14:27 +0900

    테라폼으로 HTTP 방화벽 규칙 자동화 설정 추가

--- infra/terraform/gce_k3s/main.tf
+++ infra/terraform/gce_k3s/main.tf
@@ -9,6 +9,8 @@ resource "google_compute_instance" "cloud-devsecops" {
   machine_type = "e2-micro"
   zone         = var.zone
 
+  tags = ["http-server"]
+
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2204-lts"
@@ -22,6 +24,20 @@ resource "google_compute_instance" "cloud-devsecops" {
     }
   }
 }
+
+resource "google_compute_firewall" "allow-http"{
+  name = "allow-http-traffic"
+  network = "default"
+
+  allow{
+    protocol = "tcp"
+    ports = ["80"]
+  }
+
+  target_tags = ["http-server"]
+
+  source_ranges = ["0.0.0.0/0"]
+  }
 
 output "public_ip" {
   value       = google_compute_instance.cloud-devsecops.network_interface[0].access_config[0].nat_ip
