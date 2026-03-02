7967b968b151b224b7def9673b835a5b243d4c55
dlwlsdnr <dlwlsdnr24@gmail.com>
2026-03-02 21:28:17 +0900

    정렬

--- infra/terraform/gce_k3s/variables.tf
+++ infra/terraform/gce_k3s/variables.tf
@@ -1,24 +1,24 @@
 variable "project_id" {
-    description = "GCP Project_id"
-    type = string
+  description = "GCP Project_id"
+  type        = string
 }
 
 variable "region" {
-    description = "GCP Region"
-    type = string
+  description = "GCP Region"
+  type        = string
 }
 
-variable "credentials_file" { 
-    description = "Service Account Key Path"
-    type = string
+variable "credentials_file" {
+  description = "Service Account Key Path"
+  type        = string
 }
 
 variable "instance_name" {
-    description = "Instance Name"
-    type = string
+  description = "Instance Name"
+  type        = string
 }
 
 variable "zone" {
-    description = "GCP Zone"
-    type = string
+  description = "GCP Zone"
+  type        = string
 }
